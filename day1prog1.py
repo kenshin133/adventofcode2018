22414f06202145d866487df7ba2c26c838d00de5
maddigan <bufmouse13@gmail.com>
2018-12-04 08:20:15 -0500

    added comments

--- day1prog1.py
+++ day1prog1.py
@@ -1,11 +1,21 @@
+##day1 puzzle1 of the coding advent, to help santa we have to calibrate this thing and find the frequency after all the adjustments listed in day1input
+
+#load file full of adjustments
 with open('day1input') as f:
     line = f.readline()
+    ##setup
     cnt = 1
+    frequency = 0
+
+    
     while line:
-        print(line)
+        #print(int(line))
+        ##add the adjustment to the frequency, this could be one statement but i made it two
         if line[0]=="-":
-          print("NEGATIVE")
+          frequency = frequency + int(line)
         else:
-          print("POSITIVE")
+          frequency = frequency + int(line)
+        #print the sum of all
+        print(frequency)
         line = f.readline()
         cnt += 1
--- day1prog2.py
+++ day1prog2.py
@@ -1,1 +1,21 @@
 ## this one is to find the first time a match is made between an old sum and a new some.
+
+#load file full of adjustments
+with open('day1input') as f:
+    line = f.readline()
+    ##setup
+    cnt = 1
+    frequency = 0
+
+    
+    while line:
+        #print(int(line))
+        ##add the adjustment to the frequency, this could be one statement but i made it two
+        if line[0]=="-":
+          frequency = frequency + int(line)
+        else:
+          frequency = frequency + int(line)
+        #print the sum of all
+        print(frequency)
+        line = f.readline()
+        cnt += 1
