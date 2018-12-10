62a490a5bcfc1f66b5ddd1537a42f2e41bed3d38
kenshin133 <bufmouse13@gmail.com>
2018-12-10 18:42:25 -0500

    finished day1/2

--- day1prog2.py
+++ day1prog2.py
@@ -1,25 +1,34 @@
 ## this one is to find the first time a match is made between an old sum and a new some.
 
 #load file full of adjustments
-with open('day1input') as f:
-    line = f.readline()
-    ##setup
-    cnt = 1
-    frequency = 0
-    match = 0
-    # this should keep track of previous totals
-    frequencies=[]
-    ##TODO change this to a while not matched where it will loop through the list
-    ## multiple times until it finds a match
-    while match == 0:
+##setup
+cnt = 1
+frequency = 0
+match = 0
+# this should keep track of previous totals
+frequencies=[]
+##TODO change this to a while not matched where it will loop through the list
+## multiple times until it finds a match
+while match == 0:
+    with open('day1inputtimes11') as f:
+        line = f.readline()
         while line:
             ##add the adjustment to the frequency, this could be one statement but i made it two
             frequency = frequency + int(line)
+            for fre in frequencies:
+                if str(fre) == str(frequency):
+                    match = fre
+                    print(match)
+                    exit()
             frequencies=frequencies + [frequency]
-            print(frequencies)
-            if cnt == 10:
-                match = 1
+#            print("checking " + str(len(frequencies)) + "vectors")
+            if len(frequencies) % 10000 == 0:
+                if len(frequencies) % 50000 == 0:
+                    print(frequencies)
+                print("checking " + str(len(frequencies)) + "vectors")
             cnt += 1
+            line = f.readline()
         #print the sum of all
-        print(frequency)
-        line = f.readline()
+        cnt = 1
+print(frequency)
+print("match is " + str(match))
