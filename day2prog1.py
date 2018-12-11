0a58290d8cd15112b22eb3a3ddbe6f5dc79380cf
kenshin133 <bufmouse13@gmail.com>
2018-12-11 06:53:01 -0500

    starting day2

---
+++ day2prog1.py
@@ -0,0 +1,2 @@
+##creating a checksum of all the boxes in santa's warehouse
+##data contains 250 inputs and we are checking if each letter contains two identical letters or three identical letters or both and adding those sums together.
